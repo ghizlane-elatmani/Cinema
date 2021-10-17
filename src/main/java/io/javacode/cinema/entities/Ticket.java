e97f47a8eb86cf53b50d55b3270867131daa8324
riri <ghizlane.elatmani.99@gmail.com>
2021-10-17 18:08:15 +0100

    1.11.0 - Method payerTickets()

--- src/main/java/io/javacode/cinema/entities/Ticket.java
+++ src/main/java/io/javacode/cinema/entities/Ticket.java
@@ -1,5 +1,6 @@
 package io.javacode.cinema.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class Ticket {
     @ManyToOne
     private Place place;
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Projection projection;
 
 }
--- src/main/java/io/javacode/cinema/web/CinemaRestController.java
+++ src/main/java/io/javacode/cinema/web/CinemaRestController.java
@@ -1,24 +1,28 @@
 package io.javacode.cinema.web;
 
 import io.javacode.cinema.dao.FilmRepository;
+import io.javacode.cinema.dao.TicketRepository;
 import io.javacode.cinema.entities.Film;
+import io.javacode.cinema.entities.Ticket;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CinemaRestController {
 
     @Autowired
     private FilmRepository filmRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
 
     @GetMapping(path = "/imageFilm/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] image(@PathVariable(name = "id") Long id) throws IOException {
@@ -36,5 +40,28 @@ public class CinemaRestController {
         Path path = Paths.get(System.getProperty("user.home"), "cinema", "images", photoName);
         return Files.readAllBytes(path);
     }
+
+    @PostMapping("/payerTickets")
+    public List<Ticket> payerTickets(@RequestBody TicketForm ticketForm){
+        List<Ticket> ticketList = new ArrayList<>();
+        ticketForm.getTickets().forEach(idTicket->{
+            Ticket ticket = ticketRepository.findById(idTicket).get();
+            ticket.setNameClient(ticketForm.getNomClient());
+            ticket.setReserve(true);
+            ticket.setCodePaiement(ticketForm.getCodePaiement());
+            ticketRepository.save(ticket);
+            ticketList.add(ticket);
+        });
+        return ticketList;
+    }
+
+}
+
+@Data
+class TicketForm {
+
+    private String nomClient;
+    private int codePaiement;
+    private List<Long> tickets = new ArrayList<>();
 
 }
